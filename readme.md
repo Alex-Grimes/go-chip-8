cbd0cb1f2a79f53532b6b8fdd586f287d90c17b4
alex <alexgrimes.se@gmail.com>
2023-02-10 15:17:08 -0500

    add readme template

---
+++ readme.md
@@ -0,0 +1,56 @@
+Chip 8 Emulator in Go
+
+A Chip 8 emulator built using the Go programming language. Chip 8 is an interpreted programming language that was first used on the COSMAC VIP and Telmac 1800 microcomputers in the late 1970s.
+Features
+
+    Emulates the Chip 8 instruction set
+    Supports input from the original Chip 8 hexadecimal keyboard
+    Ability to load and run Chip 8 programs
+    Option to toggle the display of the original Chip 8 screen resolution
+
+Installation
+
+To install and run the Chip 8 emulator, you will need to have Go installed on your computer.
+
+Clone the repository to your local machine:
+
+bash
+
+git clone https://github.com/alex-grimes/go-chip-8.git
+
+Change into the project directory:
+
+go
+
+cd chip8-emulator-go
+
+Build the emulator:
+
+go
+
+go build
+
+Run the emulator:
+
+go
+
+./go-chip-8 [path to Chip 8 program]
+
+Usage
+
+The Chip 8 emulator supports the original Chip 8 hexadecimal keyboard for input. The key mapping is as follows:
+
+css
+
+1 2 3 4
+q w e r
+a s d f
+z x c v
+
+You can toggle the display of the original Chip 8 screen resolution by pressing the F1 key.
+Contributing
+
+Contributions are welcome! If you would like to contribute to this project, please open a pull request.
+License
+
+This project is licensed under the MIT License.
--- vm.go
+++ vm.go
@@ -3,11 +3,6 @@ package main
 import (
 	"fmt"
 	"log"
-	"math"
-	"math/rand"
-	"os"
-	"strings"
-	"time"
 )
 
 type Display interface {
@@ -58,91 +53,91 @@ func (vm *VM) initialiseFont() {
 	vm.memory[0x052] = 0x90
 	vm.memory[0x053] = 0x90
 	vm.memory[0x054] = 0xF0
-//1
+	//1
 	vm.memory[0x055] = 0x20
 	vm.memory[0x056] = 0x60
 	vm.memory[0x057] = 0x20
 	vm.memory[0x058] = 0x20
 	vm.memory[0x059] = 0x70
-//2
+	//2
 	vm.memory[0x05A] = 0xF0
 	vm.memory[0x05B] = 0x10
 	vm.memory[0x05C] = 0xF0
 	vm.memory[0x05D] = 0x80
 	vm.memory[0x05E] = 0xF0
-//3
+	//3
 	vm.memory[0x05F] = 0xF0
 	vm.memory[0x060] = 0x10
 	vm.memory[0x061] = 0xF0
 	vm.memory[0x062] = 0x10
 	vm.memory[0x063] = 0xF0
-//4
+	//4
 	vm.memory[0x064] = 0x90
 	vm.memory[0x065] = 0x90
 	vm.memory[0x066] = 0xF0
 	vm.memory[0x067] = 0x10
 	vm.memory[0x068] = 0x10
-//5
+	//5
 	vm.memory[0x069] = 0xF0
 	vm.memory[0x06A] = 0x80
 	vm.memory[0x06B] = 0xF0
 	vm.memory[0x06C] = 0x10
 	vm.memory[0x06D] = 0xF0
-//6
+	//6
 	vm.memory[0x06E] = 0xF0
 	vm.memory[0x06F] = 0x80
 	vm.memory[0x070] = 0xF0
 	vm.memory[0x071] = 0x90
 	vm.memory[0x072] = 0xF0
-//7
+	//7
 	vm.memory[0x073] = 0xF0
 	vm.memory[0x074] = 0x10
 	vm.memory[0x075] = 0x20
 	vm.memory[0x076] = 0x40
 	vm.memory[0x077] = 0x40
-//8
+	//8
 	vm.memory[0x078] = 0xF0
 	vm.memory[0x079] = 0x90
 	vm.memory[0x07A] = 0xF0
 	vm.memory[0x07B] = 0x90
 	vm.memory[0x07C] = 0xF0
-//9
+	//9
 	vm.memory[0x07D] = 0xF0
 	vm.memory[0x07E] = 0x90
 	vm.memory[0x07F] = 0xF0
 	vm.memory[0x080] = 0x10
 	vm.memory[0x081] = 0x10
-//A
+	//A
 	vm.memory[0x082] = 0xF0
 	vm.memory[0x083] = 0x90
 	vm.memory[0x084] = 0xF0
 	vm.memory[0x085] = 0x90
 	vm.memory[0x086] = 0x90
-//B
+	//B
 	vm.memory[0x087] = 0xE0
 	vm.memory[0x088] = 0x90
 	vm.memory[0x089] = 0xE0
 	vm.memory[0x08A] = 0x90
 	vm.memory[0x08B] = 0xE0
-//C
+	//C
 	vm.memory[0x08C] = 0xF0
 	vm.memory[0x08D] = 0x80
 	vm.memory[0x08E] = 0x80
 	vm.memory[0x08F] = 0x80
 	vm.memory[0x090] = 0xF0
-//D
+	//D
 	vm.memory[0x091] = 0xE0
 	vm.memory[0x092] = 0x90
 	vm.memory[0x093] = 0x90
 	vm.memory[0x094] = 0x90
 	vm.memory[0x095] = 0xE0
-//E
+	//E
 	vm.memory[0x096] = 0xF0
 	vm.memory[0x097] = 0x80
 	vm.memory[0x098] = 0xF0
 	vm.memory[0x099] = 0x80
 	vm.memory[0x09A] = 0xF0
-//F
+	//F
 	vm.memory[0x09B] = 0xF0
 	vm.memory[0x09C] = 0x80
 	vm.memory[0x09D] = 0xF0
@@ -157,13 +152,13 @@ func (vm *VM) loadROM(rombytes []byte) {
 	}
 }
 
-fun (vm *VM) init(rombytes []byte, wrapX string, wrapY string, clockSpeed int, timerSpeed int, screenBuffer int) {
+func (vm *VM) init(rombytes []byte, wrapX string, wrapY string, clockSpeed int, timerSpeed int, screenBuffer int) {
 	vm.initialiseFont()
 	vm.loadROM(rombytes)
 	vm.pc = 0x200
 	vm.drawflag = false
 	vm.wrapX = wrapX
-	vm. wrapY = wrapY
+	vm.wrapY = wrapY
 	vm.clockSpeed = uint16(clockSpeed)
 	vm.timerSpeed = uint16(timerSpeed)
 	vm.screenBuffer = uint8(screenBuffer)
@@ -175,9 +170,9 @@ func (vm *VM) parseOpcode(keyboard Keyboard) bool {
 	vm.drawflag = false
 	switch vm.opcode & 0xF000 {
 	case 0x0000:
-		switch vm.opcode & 0x00FF{ 
+		switch vm.opcode & 0x00FF {
 		case 0x00E0:
-			//clear screen 
+			//clear screen
 			//fmt.Printf("Clear vm.screen % x, % d\n, vm.opcode, vm.pc")
 			for yp := 0; yp < 32; yp++ {
 				for xb := 0; xb < 8; xb++ {
@@ -194,7 +189,7 @@ func (vm *VM) parseOpcode(keyboard Keyboard) bool {
 			}
 			//fmt.Printf("RET pc: %x, new pc: %x\n", vm.pc, vm.stack[vm.sp]+2, vm.opcode)
 			vm.sp--
-			vm.pc = vm.stack[vm.sp] + 2 
+			vm.pc = vm.stack[vm.sp] + 2
 			// fmt.Printf("RET pc: %d/n", vm.pc)
 		default:
 			// fmt.Printf("SYS vm.opcode ignored: % x, % d\n," vm.opcode, vm.pc)
@@ -211,6 +206,24 @@ func (vm *VM) parseOpcode(keyboard Keyboard) bool {
 		// endless jumps used as halt
 
 	case 0x2000:
-		
+		// 2nnn - CALL addr
+		// Call subroutine at nnn
+		vm.stack[vm.sp] = vm.pc
+		vm.sp++
+		// fmt.Printf("CALL pc: %x, new pc: %x, opcode: %x\n", vm.pc, (oxoFFF & vm.opsode), vm.opcode)
+		vm.pc = 0x0fff & vm.opcode
+		if vm.pc < 0x200 || vm.pc > 0xFFF {
+			log.Fatal(fmt.Errorf("illegal JMP instructions - PC: %x, opcode: %x", vm.pc, vm.opcode))
+		}
+		// fmt.Printf("CALL stack: %s\n", fmt.Sprint(vm.stack))
+	case 0x3000:
+		//3xkk - SE vm.Vx, byte
+		// skip next instruction if vm.Vx = kk
+		if vm.V[0x0F00&vm.opcode>>8] == uint8(0x00FF&vm.opcode) {
+			vm.pc += 4
+		} else {
+			vm.pc += 2
+		}
+	case 0x4000:
 	}
 }
